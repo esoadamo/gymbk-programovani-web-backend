341362d44fc2b5907177bbff57330c4e745319d5
Adam Hlaváček <git@adamhlavacek.com>
2023-09-01 16:52:14 +0200

    patch(docker): save ssh keys

--- .docker/build.sh
+++ .docker/build.sh
@@ -3,5 +3,5 @@ cd "$(realpath "$(dirname "$0")")/.." &&
 # docker rmi -f ksi-be &>/dev/null &&
 docker build -f .docker/Dockerfile -t gymbk-prg . &&
 if [[ "$1" == "--run" ]]; then
-  ./.docker/start.sh "$2" "$3" "$4"
+  ./.docker/start.sh "$2" "$3" "$4" "$5"
 fi
--- .docker/start.sh
+++ .docker/start.sh
@@ -3,9 +3,11 @@
 DIR_DB="$1"
 DIR_DATA="$2"
 DIR_MODULE_LIB="$3"
+DIR_SSH="$4"
 
 [ -z "$DIR_DB" -o ! -d "$DIR_DB" ] && { echo "ERR: Given DB path is invalid"; exit 1; }
 [ -z "$DIR_DATA" -o ! -d "$DIR_DATA" ] && { echo "ERR: Given data path is invalid"; exit 1; }
+[ -z "$DIR_SSH" -o ! -d "$DIR_SSH" ] && { echo "ERR: Given SSH path is invalid"; exit 1; }
 
 echo "Selected options are"
 echo "- DB: $DIR_DB"
@@ -17,6 +19,7 @@ docker run \
   -p 3032:3032 \
   -v "$DIR_DB:/var/ksi-be.ro/" \
   -v "$DIR_DATA:/var/ksi-data.ro/" \
+  -v "$DIR_SSH:/home/ksi/.ssh/" \
   --device /dev/fuse \
   --cap-add=SYS_ADMIN \
   --privileged=true \
